632a53ca17aade68d426eec2e338655b0f1fbca4
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-16 17:29:01 +0400

    Rails view is left

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
   def index
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
   end
 
   def new
@@ -17,5 +23,15 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,18 @@ puts "seeding restaurants"
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
     category: Faker::Restaurant.type,
-    phone_number: Faker::Number.number(digits: 10)
+    phone_number: Faker::Number.number(digits: 10),
   )
-  puts "created #{restaurant.name}"
+  # puts "created #{@restaurant.name}"
 end
 puts "done"
+
+
+puts "seeding reviews"
+
+10.times do
+  Review.create!(
+    rating: rand(0..5),
+    content: Faker::Subscription.plan
+  )
+end
